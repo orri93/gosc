19b6aef6ab0874b9a0911a439babf3d6c79baaa5
Geirmundur Orri Sigurdsson <geirmundur.sigurdsson@nov.cm>
2021-03-06 22:26:43 +0100

    Adding simple nearest neighbor feature

---
+++ include/gos/nearest.h
@@ -0,0 +1,17 @@
+#ifndef GOS_NEAREST_H_
+#define GOS_NEAREST_H_
+
+#include <time.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+double gos_nearest_neighbor(double first, double second, double value);
+time_t gos_nearest_neighbor_time(time_t first, time_t second, time_t value);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/nearest.c
@@ -0,0 +1,15 @@
+#include <math.h>
+
+#include <gos/nearest.h>
+
+double gos_nearest_neighbor(double first, double second, double value) {
+  double fd = fabs(first - value);
+  double sd = fabs(second - value);
+  return fd - sd;
+}
+
+time_t gos_nearest_neighbor_time(time_t first, time_t second, time_t value) {
+  time_t fd = llabs(first - value);
+  time_t sd = llabs(second - value);
+  return fd - sd;
+}
